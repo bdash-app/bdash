cbf184416de2fcaf95f15a3ae5bd4811e933cd22
Kazuhito Hokamura <k.hokamura@gmail.com>
2016-09-05 00:48:31 +0900

    Draw chart

--- app/renderer/js/components/result_table/result_table.js
+++ app/renderer/js/components/result_table/result_table.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Plotly from 'plotly.js/dist/plotly.js';
+import yaml from 'yamljs';
 
 export default class ResultTable extends React.Component {
+  componentDidMount() {
+    this.renderChart();
+  }
+
+  componentDidUpdate() {
+    this.renderChart();
+  }
+
   render() {
     if (!this.props.query.fields || !this.props.query.rows) return null;
 
@@ -16,7 +27,26 @@ export default class ResultTable extends React.Component {
           <thead><tr>{heads}</tr></thead>
           <tbody>{rows}</tbody>
         </table>
+        <div ref="chart" />
       </div>
     );
+  }
+
+  renderChart() {
+    let m = this.props.query.sql.match(/\/\*([\s\S]*?)\*\//m);
+    if (!m) return null;
+
+    let chart = ReactDOM.findDOMNode(this.refs.chart);
+    let chartDef = yaml.parse(m[1]);
+    let params = {
+      type: chartDef.type,
+      x: this.dataByField(chartDef.x),
+      y: this.dataByField(chartDef.y),
+    };
+    Plotly.newPlot(chart, [params], {}, { displayModeBar: false });
+  }
+
+  dataByField(fieldName) {
+    return this.props.query.rows.map(r => r[fieldName]);
   }
 }
