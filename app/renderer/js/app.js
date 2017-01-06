6b6872a9c26193df404463d85e5acabe9e964e1a
Kazuhito Hokamura <k.hokamura@gmail.com>
2017-01-06 23:25:26 +0900

    Move scss to css

--- app/renderer/js/app.js
+++ app/renderer/js/app.js
@@ -3,8 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/app_container';
 
-// temporary fix
-import '../dist/css/app.css';
+import 'font-awesome/css/font-awesome.css';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/addon/dialog/dialog.css';
+import 'react-select/dist/react-select.css';
+import '../styles/app.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   let node = document.getElementById('app');
