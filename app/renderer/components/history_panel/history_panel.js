fdcadc2a5da0bbcd529b6f3c403325fc83487587
Kazuhito Hokamura <k.hokamura@gmail.com>
2017-02-03 00:03:54 +0900

    Remove unused files

--- app/renderer/components/history_panel/history_panel.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class HistoryPanel extends React.Component {
-  render() {
-    return (
-      <div className="HistoryPanel">history panel</div>
-    );
-  }
-}
