2b114aeeba2047b58eb710f001069e341a6f32af
Kazuhito Hokamura <k.hokamura@gmail.com>
2018-03-07 23:09:53 +0900

    Remove unused file

--- src/renderer/helpers/css-register.ts
+++
@@ -1,6 +0,0 @@
-require.extensions[".css"] = function(m, filename) {
-  const link = document.createElement("link");
-  link.setAttribute("rel", "stylesheet");
-  link.setAttribute("href", filename);
-  document.head.appendChild(link);
-};
