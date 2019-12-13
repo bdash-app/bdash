920a8f03a7a4d91d01f39820e2e066e764bcfc02
mtgto <hogerappa@gmail.com>
2019-12-14 08:33:13 +0900

    Add the feature to migration

--- src/lib/Database/Connection.ts
+++ src/lib/Database/Connection.ts
@@ -1,5 +1,5 @@
 import sqlite3 from "sqlite3";
-import schema from "./schema";
+import { migrations, Migration } from "./schema";
 
 export default class Connection {
   _db: any;
@@ -12,9 +12,46 @@ export default class Connection {
     return this._db;
   }
 
-  initialize({ databasePath }) {
+  async initialize({ databasePath }): Promise<void> {
     this._db = new sqlite3.Database(databasePath);
-    return this.exec(schema);
+    await this.migrate(migrations);
+  }
+
+  async migrate(migrations: Migration[]): Promise<void> {
+    await this.exec('begin;');
+    try {
+      await this.exec(
+        `create table if not exists schema_version (
+            version integer primary key
+        );`
+      );
+      const current_version: number = await this.get(`select version from schema_version`).then(
+        async (row: { version: number } | undefined) => {
+          if (row) {
+            return row.version;
+          } else {
+            await this.exec(`insert into schema_version(version) values (0);`);
+            return 0;
+          }
+        }
+      );
+
+      let last_version: number = 0;
+      for (const m of migrations) {
+        if (m.version <= last_version) {
+          throw new Error(`Wrong migration script: version ${m.version}`);
+        }
+        if (m.version > current_version) {
+          await this.exec(m.query);
+          await this.run(`update schema_version set version = ?`, m.version);
+        }
+        last_version = m.version;
+      }
+      await this.exec('commit;');
+    } catch (err) {
+      await this.exec('rollback;');
+      throw err;
+    }
   }
 
   exec(sql): Promise<any> {
