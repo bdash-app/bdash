4593684b479397bff24c0be8e61552e86e1d1ede
mtgto <hogerappa@gmail.com>
2020-03-22 22:55:23 +0900

    Use padding of GlobalMenu in any platform

--- src/renderer/components/GlobalMenu/GlobalMenu.tsx
+++ src/renderer/components/GlobalMenu/GlobalMenu.tsx
@@ -31,6 +31,6 @@ export default class GlobalMenu extends React.Component<Props> {
   }
 
   render(): React.ReactNode {
-    return <div className={classNames("GlobalMenu", { darwin: process.platform === "darwin" })}>{this.menuList}</div>;
+    return <div className="GlobalMenu">{this.menuList}</div>;
   }
 }
