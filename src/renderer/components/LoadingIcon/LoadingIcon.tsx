adb63ef58453d72bb148adb997ce1b9d589f1540
kaorun343 <5625395+kaorun343@users.noreply.github.com>
2022-05-22 02:33:44 +0900

    Refactor LoadingIcon component to functional component

--- src/renderer/components/LoadingIcon/LoadingIcon.tsx
+++ src/renderer/components/LoadingIcon/LoadingIcon.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-export default class LoadingIcon extends React.Component {
-  override render(): React.ReactNode {
-    return <i className="LoadingIcon fas fa-spin fa-sync" />;
-  }
-}
+const LoadingIcon = React.memo(function LoadingIcon() {
+  return <i className="LoadingIcon fas fa-spin fa-sync" />;
+});
+
+export default LoadingIcon;
