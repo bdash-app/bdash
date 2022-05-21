370ccbf90c36ec8bb5cca8b2bbc0d0d9ff1f6a82
kaorun343 <5625395+kaorun343@users.noreply.github.com>
2022-05-22 05:22:05 +0900

    Rewrite TableSummary component to functional component

--- src/renderer/components/TableSummary/TableSummary.tsx
+++ src/renderer/components/TableSummary/TableSummary.tsx
@@ -5,31 +5,30 @@ type Props = {
   readonly dataSource?: DataSourceType;
 };
 
-export default class TableSummary extends React.Component<Props> {
-  override render(): React.ReactNode {
-    const dataSource = this.props.dataSource;
-    if (!dataSource || !dataSource.tableSummary) return null;
+const TableSummary = React.memo<Props>(function TableSummary({ dataSource }) {
+  if (!dataSource || !dataSource.tableSummary) return null;
 
-    const tableSummary = dataSource.tableSummary;
-    const heads = tableSummary.defs.fields.map((f, i: number) => <th key={i}>{f}</th>);
-    const rows = tableSummary.defs.rows.map((row, i: number) => {
-      const cols = row.map((v, j: number) => <td key={j}>{v?.toString()}</td>);
-      return <tr key={i}>{cols}</tr>;
-    });
+  const tableSummary = dataSource.tableSummary;
+  const heads = tableSummary.defs.fields.map((f, i: number) => <th key={i}>{f}</th>);
+  const rows = tableSummary.defs.rows.map((row, i: number) => {
+    const cols = row.map((v, j: number) => <td key={j}>{v?.toString()}</td>);
+    return <tr key={i}>{cols}</tr>;
+  });
 
-    const schema = tableSummary.schema ? `${tableSummary.schema}.` : "";
-    const tableName = schema + tableSummary.name;
+  const schema = tableSummary.schema ? `${tableSummary.schema}.` : "";
+  const tableName = schema + tableSummary.name;
 
-    return (
-      <div className="TableSummary">
-        <h1 className="TableSummary-name">{tableName}</h1>
-        <table>
-          <thead>
-            <tr>{heads}</tr>
-          </thead>
-          <tbody>{rows}</tbody>
-        </table>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="TableSummary">
+      <h1 className="TableSummary-name">{tableName}</h1>
+      <table>
+        <thead>
+          <tr>{heads}</tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    </div>
+  );
+});
+
+export default TableSummary;
